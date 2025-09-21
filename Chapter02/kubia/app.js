b955427c81be0936ec3fbd7655ac1dea4a1649e0
zeonstha <57930213+zeonstha@users.noreply.github.com>
2025-09-21 17:44:07 -0400

    Update app.js

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("Hello from Kubia v0.2, running on " + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
